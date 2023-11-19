46dac740eb5969c5baba293c08bd1c25ad51a336
blo4nik <67195318+blo4nik@users.noreply.github.com>
2023-11-19 03:30:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,1 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-
-if not find_dotenv():
-    exit('Переменные окружения не загружены т.к отсутствует файл .env')
-else:
-    load_dotenv()
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN="6446904856:AAFbd1cdG_wdSamJxaMG_vrJml-y3e6yqqI"
